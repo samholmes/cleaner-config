3b9c2d806bd893b2ce977158de2d7775039bda9a
William Swanson <swansontec@gmail.com>
2022-09-13 13:33:46 -0700

    Auto-upgrade config files that do not round-trip cleanly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { asMaybe, asString, Cleaner } from 'cleaners'
+import { Cleaner, uncleaner } from 'cleaners'
 import { readFileSync, writeFileSync } from 'fs'
 import { join as joinPath } from 'path'
+
+import { matchJson } from './matchJson'
 
 export const makeConfig = <T>(
   asConfig: Cleaner<T>,
   filepath: string = 'config.json'
 ): T => {
-  let config: unknown
+  const wasConfig = uncleaner(asConfig)
+  const configPath =
+    filepath[0] === '/' ? filepath : joinPath(process.cwd(), filepath)
 
+  let config: unknown = {}
   try {
-    const json = configFromFile(filepath)
+    const json = readFileSync(configPath, 'utf8')
     config = JSON.parse(json)
   } catch (error: any) {
     if (error.code !== 'ENOENT') {
@@ -17,33 +22,25 @@ export const makeConfig = <T>(
         `Failed to read config file.\n${indentErrorStack(error.stack)}`
       )
     }
-
-    let defaultConfig: T
-    try {
-      defaultConfig = asConfig({})
-    } catch (error: any) {
-      const errorMessage =
-        asMaybe(asString)(error.message) ?? JSON.stringify(error)
-
-      throw new Error(
-        `Failed to create missing config file ${filepath} from cleaner: ${errorMessage} `
-      )
-    }
-
-    try {
-      configToFile(defaultConfig, filepath)
-    } catch (error: any) {
-      throw new Error(
-        `Failed to write config file.\n${indentErrorStack(error.stack)}`
-      )
-    }
-
-    return defaultConfig
   }
 
   // Validate config
   try {
-    return asConfig(config)
+    const clean = asConfig(config)
+    const dirty = wasConfig(clean)
+
+    // Update disk if the round-trip has differences
+    if (!matchJson(dirty, config)) {
+      try {
+        const json = JSON.stringify(dirty, null, 2)
+        writeFileSync(configPath, json, 'utf8')
+      } catch (error: any) {
+        throw new Error(
+          `Failed to write config file.\n${indentErrorStack(error.stack)}`
+        )
+      }
+    }
+    return clean
   } catch (error: any) {
     throw new TypeError(
       `Config validation failed\n${indentErrorStack(
@@ -51,21 +48,6 @@ export const makeConfig = <T>(
       )}\n\n${JSON.stringify(config)}`
     )
   }
-}
-
-const configFromFile = (filepath: string): string => {
-  const configPath =
-    filepath[0] === '/' ? filepath : joinPath(process.cwd(), filepath)
-
-  return readFileSync(configPath, 'utf8')
-}
-
-const configToFile = (config: unknown, filepath: string): void => {
-  const configPath =
-    filepath[0] === '/' ? filepath : joinPath(process.cwd(), filepath)
-  const json = JSON.stringify(config, null, 2)
-
-  writeFileSync(configPath, json, 'utf8')
 }
 
 // Utility functions:
